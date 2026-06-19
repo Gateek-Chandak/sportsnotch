60a3b80c70bd0951fbd7c8352149844def14c1b3
Gateek <chandakgateek@gmail.com>
2026-06-19 00:47:59 -0400

    fix(build script): tweak wording

--- build-app.sh
+++ build-app.sh
@@ -10,7 +10,7 @@ BUNDLE="$DIST/$APP_NAME.app"
 echo "Building release binary..."
 swift build -c release
 
-echo "Building app icon..."
+echo "Packaging icon (assets/icon.png -> .icns)..."
 rm -rf "$DIST"
 mkdir -p "$DIST/AppIcon.iconset"
 for spec in "16 16x16" "32 16x16@2x" "32 32x32" "64 32x32@2x" "128 128x128" "256 128x128@2x" "256 256x256" "512 256x256@2x" "512 512x512"; do
